7a63fa54f64ea8ef85a3d343ef0cdc3e87779d9d
WooKyoung Noh <wookay.noh@gmail.com>
2018-06-24 14:24:29 +0900

    build

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,19 @@
 using BinaryProvider
+using Compat
 
 const verbose = true #"--verbose" in ARGS
-const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
+const prefix = Prefix(joinpath(@__DIR__, "usr"))
+println("prefix ", prefix)
 
 product = LibraryProduct(prefix, "pa_ringbuffer", :libpa_ringbuffer)
-satisfied(product; verbose=verbose) && write_deps_file(joinpath(@__DIR__, "deps.jl"), [product])
+println("product ", product)
+println("satisfied ", satisfied(product; verbose=verbose))
+@static if Compat.Sys.iswindows()
+    l_path = joinpath(libdir(prefix), "libfoo.dll")
+    println("l_path ", l_path)
+    println(satisfied(l, verbose=true, platform=Windows(Sys.ARCH)))
+    satisfied(l; verbose=verbose, platform=Windows(Sys.ARCH)) && write_deps_file(joinpath(@__DIR__, "deps.jl"), [product])
+else
+    satisfied(product; verbose=verbose) && write_deps_file(joinpath(@__DIR__, "deps.jl"), [product])
+end
+println(read(joinpath(@__DIR__, "deps.jl"), String))
