3645a60d958edf68fe5e164d7942906df20e1fd9
WooKyoung Noh <wookay.noh@gmail.com>
2018-06-24 16:26:58 +0900

    win12

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ const verbose = "--verbose" in ARGS
 const prefix = Prefix(joinpath(@__DIR__, "usr"))
 
 # BinaryProvider v0.3.2 has libdir(prefix) bug on windows
-if @static Compat.Sys.iswindows()
+@static if Compat.Sys.iswindows()
     file_path = joinpath(prefix.path, "lib", string("pa_ringbuffer_", Sys.ARCH, "-w64-mingw32.dll"))
     product = FileProduct(file_path, :libpa_ringbuffer)
 else
